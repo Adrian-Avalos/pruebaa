f70cd2a7566cc243eddc61e60ada3df8f2fdb32a
Adrian-Avalos <adrianpiti28@gmail.com>
2025-08-06 11:11:56 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,21 +5,33 @@ const URLS_TO_CACHE = [
     "icon.png"
 ];
 
+// Instala y guarda en caché
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log("📦 Archivos cacheados");
-            return cache.addAll(URLS_TO_CACHE);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
+// Limpia versiones viejas
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+// Manejo de requests con fallback a index.html
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    fetch(event.request)
+      .then(networkResponse => networkResponse)
+      .catch(() => {
+        return caches.match(event.request).then(cachedResponse => {
+          return cachedResponse || caches.match("index.html");
+        });
+      })
+  );
 });
 
 
